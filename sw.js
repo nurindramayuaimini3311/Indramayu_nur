cc8709a748ce7a49d8eb8a31672a1b288f52055b
Dul Kohar <dulkohar@example.com>
2026-08-09 05:58:32 +0700

    fix: manifest valid & hapus rewrite 500

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ const CACHE = "indramayu-v1";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/img/icon-192.png", "/img/icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
-  self.skipWaiting();
 });
 self.addEventListener("fetch", e => {
   e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
