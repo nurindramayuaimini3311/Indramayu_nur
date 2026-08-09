690fee473dc438ef6f49e9610c42a03ff8d9ea91
Dul Kohar <dulkohar@example.com>
2026-08-09 09:38:31 +0700

    fix: nav inline kiri kanan + sw reset

--- sw.js
+++ sw.js
@@ -1,8 +1,3 @@
-const CACHE = "indramayu-v1";
-const ASSETS = ["/", "/index.html", "/manifest.json", "/img/icon-192.png", "/img/icon-512.png"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
+self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c))))));
+self.addEventListener('fetch', e=>e.respondWith(fetch(e.request)));
