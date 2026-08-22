cc61f323979bf4943d669a09ef619c6bd0789211
Dul Kohar <dulkohar@example.com>
2026-08-22 20:57:27 +0700

    feat: sw.js v2 offline + auto-update for meta_folder VPS connect

--- sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', e=>self.skipWaiting());
-self.addEventListener('activate', e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c))))));
-self.addEventListener('fetch', e=>e.respondWith(fetch(e.request)));
