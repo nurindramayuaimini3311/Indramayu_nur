e73b88b086724fcfa560df1aa9df1e4d68045fce
Jamhari Dulkohar <dkohar011@gmail.com>
2026-09-08 02:29:29 +0000

    add bubble css, alwi_bubble4, PWA kartu + manifest/sw, icon img, ADZAN mp3, wrangler config
    - css/style.css (bubble background) ke promosi-streaming & gambar-streaming + NURgenerator
    - alwi_bubble4.js (versi fix goHome + icon absolut NURgenerator/img/icon-72.png)
    - pwa.html (9 kartu menu) + manifest.json (1 icon 512) + sw.js
    - NURgenerator/img/ (icon-72, 192, 512)
    - NURgenerator/wrangler.jsonc (deploy fancy-wood-77c8)
    - NURgenerator/ADZAN/ (19 file mp3 adzan)
    - Inject bubble + alwi_bubble4 ke index utama & 12 file promosi/gambar-streaming
    - Deploy worker fancy-wood-77c8 selesai (version fd3d67a5)

---
+++ NURgenerator/sw.js
@@ -0,0 +1,55 @@
+const ALWI_CACHE = 'nurg-pwa-v1';
+const CORE = [
+  './',
+  './pwa.html',
+  './manifest.json',
+  './css/style.css'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(ALWI_CACHE).then(function(c){
+      return c.addAll(CORE);
+    }).then(function(){ return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(k){ return k !== ALWI_CACHE; })
+            .map(function(k){ return caches.delete(k); })
+      );
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  var url = new URL(e.request.url);
+  if (e.request.method !== 'GET') return;
+  if (url.origin !== location.origin) return;
+
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(function(){
+        return caches.match('./pwa.html');
+      })
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(function(m){
+      if (m) return m;
+      return fetch(e.request).then(function(res){
+        var ok = res && (res.status === 200 || res.type === 'basic');
+        if (ok) {
+          var cp = res.clone();
+          caches.open(ALWI_CACHE).then(function(c){ c.put(e.request, cp); });
+        }
+        return res;
+      });
+    })
+  );
+});
